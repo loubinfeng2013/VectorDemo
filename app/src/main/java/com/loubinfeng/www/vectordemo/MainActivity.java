a2a7095c7a1c510f9dffe6777ed3fac27a36c997
loubinfeng <loubinfeng2013@163.com>
2017-03-26 17:55:37 +0800

    动态Vector使用

--- app/src/main/java/com/loubinfeng/www/vectordemo/MainActivity.java
+++ app/src/main/java/com/loubinfeng/www/vectordemo/MainActivity.java
@@ -1,8 +1,12 @@
 package com.loubinfeng.www.vectordemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handleDownloadAnim(View v) {
+        Log.i("info","handleDownloadAnim");
+        ImageView imageView = (ImageView) v;
+        Drawable drawable = imageView.getDrawable();
+        if (drawable instanceof Animatable)
+            ((Animatable)drawable).start();
     }
 }
